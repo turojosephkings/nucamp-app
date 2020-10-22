3f3cbc4d82963df1a959effbfbf4b81561319795
turojosephkings <turojkings@gmail.com>
2020-10-22 19:35:24 -0400

    10th Commit - Activity Indicator

--- components/MissionComponent.js
+++ components/MissionComponent.js
@@ -6,7 +6,7 @@ import { Card } from 'react-native-elements';
 function Mission() {
 
     return (
-        <Card>
+        <Card title='Our Mission'>
             <Text style={{margin:10}}>
                 We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
             </Text>
