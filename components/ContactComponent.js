0f0062e44bd3dc25d2ec368957e54832ed7a5c36
turojosephkings <turojkings@gmail.com>
2020-10-29 14:40:43 -0400

    18th Commit - Animatable

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Text, View, ScrollView } from 'react-native';
 import { Card } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
 
 class Contact extends Component {
 
@@ -12,24 +13,26 @@ class Contact extends Component {
         
         return(
             <ScrollView>
-                <Card
-                title={"Contact Information"}
-                wrapperStyle={{margin: 20}}>
-                    <Text 
-                    style={{marginBottom: 10}}>
-                        1 Nucamp Way
-                        {"\n"}
-                        Seattle, WA 98001
-                        {"\n"}
-                        U.S.A.
-                    </Text>
-                    <Text>
-                            Phone: 1-206-555-1234
-                    </Text>
-                    <Text>
-                            Email: campsites@nucamp.co
-                    </Text>
-                </Card>            
+                <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                    <Card
+                    title={"Contact Information"}
+                    wrapperStyle={{margin: 20}}>
+                        <Text 
+                        style={{marginBottom: 10}}>
+                            1 Nucamp Way
+                            {"\n"}
+                            Seattle, WA 98001
+                            {"\n"}
+                            U.S.A.
+                        </Text>
+                        <Text>
+                                Phone: 1-206-555-1234
+                        </Text>
+                        <Text>
+                                Email: campsites@nucamp.co
+                        </Text>
+                    </Card>            
+                </Animatable.View>
             </ScrollView>
         )
     }
