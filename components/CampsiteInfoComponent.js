fffdccd76ac55d9644b4c13491f25d544f9d6b36
turojosephkings <turojkings@gmail.com>
2020-11-04 21:20:48 -0500

    25th Commit - Share

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList, Modal, Button, StyleSheet, Alert, PanResponder } from 'react-native';
+import { Text, View, ScrollView, FlatList, Modal, Button, StyleSheet, Alert, PanResponder, Share } from 'react-native';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -57,15 +57,22 @@ function RenderCampsite(props) {
                     ],
                     { cancelable: false }
                 );
-            } else {
-                if (recognizeComment(gestureState)) {
+            } else if (recognizeComment(gestureState)) {
                     props.onShowModal();
-                }
-            } 
-                
+                } 
             return true;
         }
     });
+
+    const shareCampsite = (title, message, url) => {
+        Share.share({
+            title,
+            message: `${title}: ${message} ${url}`,
+            url
+        },{
+            dialogTitle: 'Share '+ title
+        })
+    }
 
     if (campsite) {
         return (
@@ -100,6 +107,14 @@ function RenderCampsite(props) {
                                 reverse
                                 onPress={() => props.onShowModal()}
                             />
+                            <Icon
+                                name='share'
+                                type='font-awesome'
+                                color='#5637DD'
+                                raised
+                                reverse
+                                onPress={() => shareCampsite(campsite.name, campsite.description, baseUrl + campsite.image)}
+                            />                            
                         </View>
                 </Card>  
             </Animatable.View>    
